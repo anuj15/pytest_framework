e5bd6e715f7d5270f6ee018eba07206d5505766f
pradeep_kumar_sharma <pradeep.kumar.sharma@spglobal.com>
2024-12-17 11:49:03 +0530

    Adding common methods and utilities

--- pages/home_page.py
+++ pages/home_page.py
@@ -1,13 +1,20 @@
 import selenium
 from selenium.webdriver.common.by import By
 
+from utilities.common_methods import common_methods
 
-
-
-class HomePage:
+class HomePage(common_methods):
     # Defining locators
     this_is_a_link = "//a[text()='This is a link']"
 
     def __init__(self):
-        pass
+        super().__init__()
+
+
+    def navigate_to_home_page(self):
+        self.launch_browser()
+        self.driver.get("https://artoftesting.com/samplesiteforselenium")
+
+
+
 
--- tests/step_definitions/test_practice_steps.py
+++ tests/step_definitions/test_practice_steps.py
@@ -1,6 +1,10 @@
+import time
+
 from pytest_bdd import given, when, then, scenario
 
+from pages import home_page
 
+homepage = home_page.HomePage()
 @scenario(r'../features/automation_practice.feature', 'Check the link is working on the page')
 def test_scenario():
     pass
@@ -8,12 +12,13 @@ def test_scenario():
 
 @given('User is on Homepage')
 def i_am_on_the_practice_page():
-    pass
+    homepage.navigate_to_home_page()
 
 
 @when('user click on the link')
 def i_click_on_the_link():
-    pass
+    homepage.click_on_element("//button[contains(text(), 'Generate Alert Box')]")
+    time.sleep(20)
 
 
 @then('link text should be printed')
